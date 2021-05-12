4bc8c37e99860b251224d7dd2a8fdb4c0b885789
Lucas Rodrigues <lucas.rbit@gmail.com>
2021-05-12 20:09:10 -0300

    add buttons alinhados usando baselines

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text)
+                box_four_text, box_five_text, constraint_layout,
+                red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -27,14 +28,16 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
 
-            // Boxes using Color class colors for background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
-            // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
